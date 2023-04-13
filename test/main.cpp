2c7f69b5f4b54f097cd559a7ff3e8595fbd01561
zhouzilong <529620861@qq.com>
2023-04-13 09:10:18 -0400

    feat: more test

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,6 @@
  * This file is used to launch the experiments.
  */
 
-// TODO: correct the include path
 #include <log.h>
 #include <log_kv.h>
 #include <naive_kv.h>
@@ -11,6 +10,8 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+
+using namespace std::chrono;
 
 std::string randomString(int strLen)
 {
@@ -31,9 +32,12 @@ bool cmpTables(LogKV &logKV, std::unordered_map<std::string, std::string> &map)
     assert(logKV.size() == map.size());
     for (auto kv : map)
     {
-        if (kv.second != logKV.get(kv.first))
+        auto ptr = logKV.get(kv.first);
+        if (ptr == nullptr || kv.second != *ptr)
         {
-            printf("wrong val\n");
+            std::cerr << "Wrong val, expected\n"
+                      << kv.second << "\ngot\n"
+                      << *ptr << std::endl;
             return false;
         }
     }
@@ -52,15 +56,10 @@ void testBasicGetPut()
         std::string key = randomString(25);
         std::string val = randomString(50);
         map[key] = val;
-        printf("put %s %s\n", key.c_str(), val.c_str());
-
         logKV.put(key, val);
     }
 
-    if (!cmpTables(logKV, map))
-    {
-        exit(1);
-    }
+    assert(cmpTables(logKV, map));
 
     printf("Succeed!\n");
 }
@@ -81,59 +80,117 @@ void testBasicDelete()
         map[key] = val;
         logKV.put(key, val);
         allKey.push_back(key);
-        printf("put %s %s\n", key.c_str(), val.c_str());
+        // printf("put %s %s\n", key.c_str(), val.c_str());
     }
 
     std::vector<std::string>::iterator it;
 
     // remove first
     it = allKey.begin();
-    printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
+    // printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
     map.erase(*it);
     logKV.deleteK(*it);
     allKey.erase(it);
-    if (!cmpTables(logKV, map)) exit(1);
+    assert(cmpTables(logKV, map));
 
     // remove last
     it = allKey.end() - 1;
-    printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
+    // printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
     map.erase(*it);
     logKV.deleteK(*it);
     allKey.erase(it);
-    if (!cmpTables(logKV, map)) exit(1);
+    assert(cmpTables(logKV, map));
 
     // remove mid
     it = allKey.begin() + 5;
-    printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
+    // printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
     map.erase(*it);
     logKV.deleteK(*it);
     allKey.erase(it);
-    if (!cmpTables(logKV, map)) exit(1);
+    assert(cmpTables(logKV, map));
 
     // remove all
     for (it = allKey.begin(); it < allKey.end(); it++)
     {
-        printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
+        // printf("delete %s %s\n", (*it).c_str(), (map[*it]).c_str());
         map.erase(*it);
         logKV.deleteK(*it);
-        if (!cmpTables(logKV, map)) exit(1);
+        assert(cmpTables(logKV, map));
     }
+
+    printf("Succeed!\n");
+}
+
+void testAdvanced()
+{
+    printf("testing testAdvanced (2M kv with 0.1 deletes 0.2 updates)\n");
+
+    LogKV logKV;
+    std::unordered_map<std::string, std::string> map;
+    std::vector<std::string> keys;
+
+    // 2M entries
+    for (int i = 0; i < (1 << 20); i++)
+    {
+        std::string key = randomString(25);
+        std::string val = randomString(50);
+        keys.push_back(key);
+        map[key] = val;
+        logKV.put(key, val);
+
+        // 10% chance to delete
+        if (arc4random() % 100 < 10)
+        {
+            int idx = arc4random() % keys.size();
+            auto key = keys[idx];
+            keys.erase(keys.begin() + idx);
+            map.erase(key);
+            logKV.deleteK(key);
+        }
+
+        // 20% chance to update
+        if (arc4random() % 100 < 20)
+        {
+            int idx = arc4random() % keys.size();
+            auto key = keys[idx];
+            std::string newVal = randomString(50);
+            logKV.put(key, newVal);
+            map[key] = newVal;
+        }
+    }
+
+    assert(cmpTables(logKV, map));
+
+    printf("Succeed!\n");
+}
+
+void testBigKV()
+{
+    printf("testing testBigKV\n");
+
+    LogKV logKV;
+    std::unordered_map<std::string, std::string> map;
+
+    // 1k entries
+    for (int i = 0; i < (1 << 10); i++)
+    {
+        std::string key = randomString(200);
+        std::string val = randomString(5000);
+        map[key] = val;
+        logKV.put(key, val);
+    }
+
+    assert(cmpTables(logKV, map));
 
     printf("Succeed!\n");
 }
 
 int main(int argc, char *argv[])
 {
-    try
-    {
-        setClock();
-        testBasicGetPut();
-        testBasicDelete();
-        printf("Duration %d\n", getOpTime());
-    }
-    catch (const std::exception &ex)
-    {
-        fprintf(stderr, "%s", ex.what());
-    }
+    testBasicGetPut();
+    testBasicDelete();
+    testAdvanced();
+    testBigKV();
+
     return 0;
 }
